e2ff876b02d7ac4da0f474c22b92d87b021ad0bc
Zach George <zachg4393@gmail.com>
2025-07-29 21:42:19 -0500

    Phase 1 Step 3. demonstrate scaling and rotations and translating. Including use of homogenous coordinates to apply the three in one shot

--- phase1/src/cube_demo.py
+++ phase1/src/cube_demo.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 cube_points = np.array([
-    [1,1,1],
-    [1,1,-1],
-    [1,-1,1],
-    [1,-1,-1],
-    [-1,1,1],
-    [-1,1,-1],
-    [-1,-1,1],
-    [-1,-1,-1]
+    [1,1,1,1],
+    [1,1,-1,1],
+    [1,-1,1,1],
+    [1,-1,-1,1],
+    [-1,1,1,1],
+    [-1,1,-1,1],
+    [-1,-1,1,1],
+    [-1,-1,-1,1]
     ])
 
 # Define cube edges by point indices
@@ -23,24 +23,116 @@ edges = [
     (6, 7)
 ]
 
-# Plotting
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Eventually i want to come back here and use lin alg principles of
+# homogenous coordinates to use a 4x4 matrix to rotate scale and translate
+# at the same time
 
-# Plot points
-ax.scatter(cube_points[:, 0], cube_points[:, 1], cube_points[:, 2], color='red')
+def scale(arr, x):
+    for coordinates in arr:
+        coordinates[0] *= x
+        coordinates[1] *= x
+        coordinates[2] *= x
+        
+def rotatearoundz(arr):
+    rotationmatrix = np.array([
+        [0,1,0],
+        [-1,0,0],
+        [0,0,1]
+    ])
+    rotated = arr @ rotationmatrix.T
+    return rotated
 
-# Draw edges
-for start, end in edges:
-    xs = [cube_points[start][0], cube_points[end][0]]
-    ys = [cube_points[start][1], cube_points[end][1]]
-    zs = [cube_points[start][2], cube_points[end][2]]
-    ax.plot(xs, ys, zs, color='blue')
+def homo(arr):
+    matrix = np.array([
+        [2,0,0,5],
+        [0,2,0,-5],
+        [0,0,2,0],
+        [0,0,0,1]
+    ])
+    rotated = arr @ matrix.T
+    return rotated
 
-# Labels and aspect
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_box_aspect([1, 1, 1])  # Equal aspect ratio
 
-plt.show()
+
+
+def plotcubetemp():
+    # Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot points
+    ax.scatter(cube_points[1:, 0], cube_points[1:, 1], cube_points[1:, 2], color='red')
+    ax.scatter(cube_points[0, 0], cube_points[0, 1], cube_points[0, 2], color='green')
+
+    # Draw edges
+    for start, end in edges:
+        xs = [cube_points[start][0], cube_points[end][0]]
+        ys = [cube_points[start][1], cube_points[end][1]]
+        zs = [cube_points[start][2], cube_points[end][2]]
+        ax.plot(xs, ys, zs, color='blue')
+
+    # Labels and aspect
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_box_aspect([1, 1, 1])  # Equal aspect ratio
+
+    plt.show(block = False)
+    plt.pause(3)
+    plt.close()
+    
+def plotcube():
+    # Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot points
+    ax.scatter(cube_points[1:, 0], cube_points[1:, 1], cube_points[1:, 2], color='red')
+    ax.scatter(cube_points[0, 0], cube_points[0, 1], cube_points[0, 2], color='green')
+
+    # Draw edges
+    for start, end in edges:
+        xs = [cube_points[start][0], cube_points[end][0]]
+        ys = [cube_points[start][1], cube_points[end][1]]
+        zs = [cube_points[start][2], cube_points[end][2]]
+        ax.plot(xs, ys, zs, color='blue')
+
+    # Labels and aspect
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_box_aspect([1, 1, 1])  # Equal aspect ratio
+
+    plt.show()
+    
+plotcubetemp()
+        
+# scale(cube_points, 3)
+
+# plotcubetemp()
+
+# scale(cube_points, 1/3)
+
+# plotcubetemp()
+
+# cube_points = rotatearoundz(cube_points)
+
+# plotcubetemp()
+
+# cube_points = rotatearoundz(cube_points)
+
+# plotcubetemp()
+
+# cube_points = rotatearoundz(cube_points)
+
+# plotcubetemp()
+
+# cube_points = rotatearoundz(cube_points)
+
+# plotcubetemp()
+
+# cube_points += [1,1,1]
+
+cube_points = homo(cube_points)
+
+plotcube()
